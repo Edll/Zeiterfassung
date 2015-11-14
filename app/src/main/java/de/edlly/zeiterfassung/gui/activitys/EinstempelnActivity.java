025578cd6c2d6a29dc4f1fdc491c36bde5cc790d
Edlly <java@edlly.de>
2015-11-14 17:33:28 +0100

    Add - Service Binder Handler
    Erste Version von einem Service mit einem Handler für Callback Action

--- app/src/main/java/de/edlly/zeiterfassung/gui/activitys/EinstempelnActivity.java
+++ app/src/main/java/de/edlly/zeiterfassung/gui/activitys/EinstempelnActivity.java
@@ -2,8 +2,13 @@ package de.edlly.zeiterfassung.gui.activitys;
 
 
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,18 +17,45 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import de.edlly.zeiterfassung.R;
+import de.edlly.zeiterfassung.controller.StempelService;
 import de.edlly.zeiterfassung.model.IUpdateTask;
 import de.edlly.zeiterfassung.controller.Uhr;
+import de.edlly.zeiterfassung.model.stempeln.IStempelOrt;
+import de.edlly.zeiterfassung.model.stempeln.Stempel;
+import de.edlly.zeiterfassung.model.stempeln.StempelOrt;
+import de.edlly.zeiterfassung.model.zeit.AktuelleZeit;
 
 public class EinstempelnActivity extends Activity implements IUpdateTask {
     private Uhr uhr;
+    private static Handler stempelCallbackHandler = new Handler();
+    private StempelService.StempelServiceBinder stempelBinder;
+
+    private ServiceConnection stempelService = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            stempelBinder =(StempelService.StempelServiceBinder) service;
+            stempelBinder.setCallbackHandler(stempelCallbackHandler);
+            stempelBinder.setRunnable(new RunnableStempel());
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         uhr = new Uhr(this);
         Uhr();
+
+        final Intent stempelIntent = new Intent(this, StempelService.class);
+
+        bindService( stempelIntent,  stempelService, BIND_AUTO_CREATE);
     }
+
 
     @Override
     protected void onStart() {
@@ -32,11 +64,31 @@ public class EinstempelnActivity extends Activity implements IUpdateTask {
     }
 
     public void einstempeln() {
-        Toast.makeText(this, "Eingestempelt", Toast.LENGTH_SHORT).show();
+        Stempel stempel = new Stempel();
+        stempel.set(new AktuelleZeit(), IStempelOrt.StempelArt.KOMMEN, new StempelOrt()) ;
 
-        Intent intent = new Intent(this, AusstempelnActivity.class);
-        startActivity(intent);
-        finish();
+        stempelBinder.stempeln(stempel);
+       // Toast.makeText(this, "Eingestempelt", Toast.LENGTH_SHORT).show();
+
+      //  Intent intent = new Intent(this, AusstempelnActivity.class);
+      //  startActivity(intent);
+      //  finish();
+    }
+
+    public class RunnableStempel implements Runnable{
+        private Stempel stempelGet;
+
+        public void setStempelGet(Stempel stempelGet) {
+            this.stempelGet = stempelGet;
+        }
+
+        @Override
+        public void run() {
+
+            final TextView uhrzeit = (TextView) findViewById(R.id.textUhrzeit);
+            uhrzeit.setText("FUCK Service Läuft!");
+
+        }
     }
 
     @Override
@@ -60,6 +112,15 @@ public class EinstempelnActivity extends Activity implements IUpdateTask {
         uhr.stopUhr();
     }
 
+    @Override
+    protected void onDestroy(){
+        stempelCallbackHandler.removeCallbacksAndMessages(null);
+        unbindService(stempelService);
+        stopService(new Intent(this, StempelService.class));
+
+
+        super.onDestroy();
+    }
 
     @Override
     public void update() {
